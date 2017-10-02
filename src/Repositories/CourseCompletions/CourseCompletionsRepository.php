61999f44ffbe64c6c072a629ba1ceb8163af19c6
Alekhya <balekhya@contourgroupinc.com>
2017-10-02 18:54:16 +0530

    Added edit, update, destroy functions

--- src/Repositories/CourseCompletions/CourseCompletionsRepository.php
+++ src/Repositories/CourseCompletions/CourseCompletionsRepository.php
@@ -39,17 +39,38 @@ class CourseCompletionsRepository implements CourseCompletionsInterface
 
   public function edit($id)
   {
+    $course_completion = CourseCompletion::with(['course','user'])->findOrFail($id);
+    $users = User::all();
+    $courses = Course::all();
 
+    return view('vendor.LmsApi.course_completions.edit')
+            ->with('course_completion',$course_completion)
+            ->with('users',$users)
+            ->with('courses',$courses);
   }
 
   public function update(CourseCompletions\UpdateRequest $request,$id)
   {
-
+    $input = $request->all();
+    $course_completion = CourseCompletion::findOrFail($id);
+    $course_completion->user_id = $input['user_id'];
+    $course_completion->course_id = $input['course_id'];
+    $course_completion->status = $input['status'];
+    $course_completion->score = $input['score'];
+    $course_completion->issued_certificate = $input['issued_certificate'];
+    $course_completion->from_timestamp = $input['from_timestamp'];
+    $course_completion->to_timestamp = $input['to_timestamp'];
+    $course_completion->save();
+    \Session::flash('success','Course Completions updated successfully');
+    return redirect('lms\course\completions');
   }
 
   public function destroy($id)
   {
-
+      $course_completion = CourseCompletion::findOrFail($id);
+      $course_completion->delete();
+      \Session::flash('success','Course Completions deleted successfully');
+      return redirect('lms\course\completions');
   }
 
 }
